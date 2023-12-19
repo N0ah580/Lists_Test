7ebf00339957624219aa98070e14b632e36555be
N0ah580 <145355575+N0ah580@users.noreply.github.com>
2023-12-19 09:13:47 -0500

    THIS WAS OK

--- store.py
+++ store.py
@@ -1,3 +1,24 @@
-# Programmer: 
-# Description: 
+# Programmer: Noah
+# Description: Store_Test
+
+
+#Ask for the amount of item wanted to purchase
+
+amount = int(input ("How many items are you purchasing? "))
+
+count = 1
+subtotal = 0
+
+#Create a function to ask for and calculate prices
+while count <= amount:
+    price = float(input ("Enter item 1 price: "))
+    subtotal += price
+    count += 1
+
+#print combined total and apply tax
+print (f"Subtotal: ${subtotal:,.2f}")
+tax = subtotal * 0.13
+print (f"Tax: ${tax:,.2f}")
+total = subtotal + tax
+print (f"Total: ${total:,.2f}")
 
